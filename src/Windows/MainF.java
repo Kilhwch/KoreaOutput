7c1d4dc0c6c7874a1d954e88fa9a5e1920a7181d
Kasper Kivikataja <kasper.kivikataja@gmail.com>
2015-05-19 10:10:54 -0300

    main-study link

--- src/Windows/MainF.java
+++ src/Windows/MainF.java
@@ -1,9 +1,12 @@
 package Windows;
 
 import Files.FileNames;
+import Windows_Study.StudyF;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
@@ -14,11 +17,11 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
-import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 public class MainF extends JPanel {
 
+    protected static final int _TXT = 4;
     protected static final int WINDOW_WIDTH = 500;
     protected static final int WINDOW_HEIGHT = 500;
     protected static final String ICONPATH = "./Files/Icons/main.jpg";
@@ -26,20 +29,20 @@ public class MainF extends JPanel {
     
     
     public static void main(String[] args) {
-        JFrame f = new JFrame(TITLE);
-        JPanel pane = new MainF(f);
-        f.add("Center", pane);
+        final JFrame frame = new JFrame(TITLE);
+        JPanel pane = new MainF(frame);
+        frame.add("Center", pane);
         
         ImageIcon icon = new ImageIcon(ICONPATH);
-        f.setIconImage(icon.getImage());
-        f.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setLocationRelativeTo(null);
-        f.setResizable(true);
-        f.setVisible(true);
+        frame.setIconImage(icon.getImage());
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setVisible(true);
     }
     
-    public MainF(JFrame f) {
+    public MainF(final JFrame frame) {
         GridBagLayout gridbag = new GridBagLayout();
         GridBagConstraints c = new GridBagConstraints();
         setLayout(gridbag);
@@ -47,16 +50,16 @@ public class MainF extends JPanel {
         c.anchor = GridBagConstraints.CENTER;
         c.gridx = 1;
         c.gridy = 1;
+        
+        
         DefaultListModel model = new DefaultListModel();
-        
-        
-        File[] files = new FileNames().getAll();
+        final File[] files = new FileNames().getAll();
         
         for (File file : files) {
-            model.addElement(file.getName().substring(0, file.getName().length() - 4));
+            model.addElement(file.getName().substring(0, file.getName().length() - _TXT));
         }
         
-        JList list = new JList(model);
+        final JList list = new JList(model);
         DefaultListCellRenderer renderer =  (DefaultListCellRenderer)list.getCellRenderer();  
         renderer.setHorizontalAlignment(SwingConstants.CENTER);
         
@@ -74,6 +77,18 @@ public class MainF extends JPanel {
         c.gridy = 2;
         gridbag.setConstraints(start, c);
         add(start);
+        
+        
+        start.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (list.getSelectedIndex() != -1) {
+                    StudyF study = new StudyF(files[list.getSelectedIndex()].getName());
+                    frame.setVisible(false);
+                    study.windowInit();
+                }
+            }
+        });
     }
 }
 
